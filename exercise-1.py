fa263a9084fb9e613e482efce86448c414f3ef0d
zackbeckwith <z.beckwith99@gmail.com>
2022-04-04 20:18:50 -0400

    exercise 1

--- exercise-1.py
+++ exercise-1.py
@@ -10,3 +10,9 @@
 
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
+
+letter = input("Please enter a letter from the alphabet (a-z or A-Z):").lower()
+if letter in 'a, e, i, o, u':
+  print(f'The letter {letter} is a vowel.')
+else:
+  print(f'The letter {letter} is a consonant.')
